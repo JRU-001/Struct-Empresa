e236955ba3f2a872060e518bee49305ab377234d
TestBed-01 <TestBed-01@192.168.1.9>
2018-03-05 18:12:45 -0400

    .

--- EmpresaStruc/src/Logic/Empleado.java
+++ EmpresaStruc/src/Logic/Empleado.java
@@ -2,47 +2,47 @@ package Logic;
 
 public abstract class Empleado {
 	
-	protected double salarioBase;
-	protected String nombre;
-	protected String jefe;
-	protected String IdEmpleado;
+	public double salarioBase;
+	public String nombre;
+	public String jefe;
+	public String IdEmpleado;
 		
 	public Empleado(String nombre, double salarioBase) {
 		//
 	}
 	 
 	 
-	protected boolean equals(Empleado otro) {
+	public boolean equals(Empleado otro) {
 		
 	}
 	//retorna “true” si los dos empleados tienen el mismo Id y “false” en caso contrario. 
 	 
 	
-	protected String toString() {
+	public String toString() {
 		//
 	}
 	//Retorna un String compuesto por el Id y el nombre del empleado. 
 	 
 	
-	protected String estatusEmpleado() {
+	public String estatusEmpleado() {
 		//
 	}
 	//Retorna un String que contiene el estatus del empleado.  Es diferente para cada subclase de empleado. 
 
 
-	protected double getSalarioBase() {
+	public double getSalarioBase() {
 		return salarioBase;
 	}
 
-	protected String getNombre() {
+	public String getNombre() {
 		return nombre;
 	}
 
-	protected String getJefe() {
+	public String getJefe() {
 		return jefe;
 	}
 
-	protected String getIdEmpleado() {
+	public String getIdEmpleado() {
 		return IdEmpleado;
 	}
 	
